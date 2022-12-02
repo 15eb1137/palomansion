daffcb5757e849ad90127e2d0398a31547fe14cd
iori tobeta <15eb1137@gmail.com>
2022-12-03 03:24:24 +0900

    panel widget init

---
+++ flutter/lib/panel.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class Panel extends StatelessWidget {
+  const Panel(this.decibel, {super.key});
+
+  final double decibel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+        children: [
+          SfCartesianChart(),
+          const Text(''), // Big dB
+          const Text(''), // Detail Avg & Max dB
+          const Text('')  // dB size description
+        ]);
+  }
+}
