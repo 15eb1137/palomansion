3369f6054d7428d523f7140af16a59eb81d2ea11
iori tobeta <15eb1137@gmail.com>
2022-12-03 20:34:33 +0900

    define x as decimal

--- flutter/lib/panel.dart
+++ flutter/lib/panel.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+typedef Decibel = int;
+
 class Panel extends StatelessWidget {
   const Panel(this.decibel, {super.key});
 
@@ -9,20 +11,35 @@ class Panel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
-      SfCartesianChart(title: ChartTitle(text: ''), series: getDefaultData()),
+      SfCartesianChart(title: ChartTitle(text: ''), series: getData()),
       const Text(''), // Big dB
       const Text(''), // Detail Avg & Max dB
       const Text('') // dB size description
     ]);
   }
 
-  static List<ColumnSeries<num, num>> getDefaultData() {
-    final data = <num>[1, 2, 3, 4, 5];
-    return <ColumnSeries<num, num>>[
+  static List<ColumnSeries<NoiseLv, Decibel>> getData() {
+    final graphShape = <NoiseLv>[
+      NoiseLv(50, 1),
+      NoiseLv(60, 2),
+      NoiseLv(70, 4),
+      NoiseLv(80, 8),
+      NoiseLv(90, 16),
+      NoiseLv(100, 32),
+      NoiseLv(110, 64),
+      NoiseLv(120, 128)
+    ];
+    return <ColumnSeries<NoiseLv, Decibel>>[
       ColumnSeries(
-          dataSource: data,
-          xValueMapper: (datum, index) => datum,
-          yValueMapper: (datum, index) => datum)
+          dataSource: graphShape,
+          xValueMapper: (datum, _) => datum.decibel,
+          yValueMapper: (datum, _) => datum.num)
     ];
   }
+}
+
+class NoiseLv {
+  NoiseLv(this.decibel, this.num);
+  final int decibel;
+  final double num;
 }
