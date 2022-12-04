e321c734b59e6821d94053d9452d56a4ce99faa1
iori tobeta <15eb1137@gmail.com>
2022-12-04 11:14:34 +0900

    Panel test init

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:palomansion/main.dart';
+import 'package:palomansion/panel.dart';
 
-// import 'test_data.dart';
+import 'test_tools.dart';
 
 void main() {
-  testWidgets('', (tester) async {});
+  testWidgets('Panel test', (tester) async {
+    await tester.pumpWidget(wrap(const MainPage()));
+    expect(find.byType(Panel), findsOneWidget);
+  });
 }
