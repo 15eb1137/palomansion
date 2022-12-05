e03493e416addb723a3500f61e01895081ccd3f5
iori tobeta <15eb1137@gmail.com>
2022-12-06 05:27:03 +0900

    add noise_meter learning test

--- flutter/test/test_tools.dart
+++ flutter/test/test_tools.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 Widget wrap(Widget child) {
   return MaterialApp(home: Material(child: child));
+}
+
+void initializeFakeSensorChannel(String channelName, List<double> data) {
+  const standardMethod = StandardMethodCodec();
+
+  void emitEvent(ByteData? event) {
+    ServicesBinding.instance.defaultBinaryMessenger.handlePlatformMessage(
+      channelName,
+      event,
+      (reply) {},
+    );
+  }
+
+  TestDefaultBinaryMessengerBinding.instance!.defaultBinaryMessenger
+      .setMockMessageHandler(channelName, (message) async {
+    final methodCall = standardMethod.decodeMethodCall(message);
+    if (methodCall.method == 'listen') {
+      emitEvent(standardMethod.encodeSuccessEnvelope(data));
+      emitEvent(null);
+      return standardMethod.encodeSuccessEnvelope(null);
+    } else if (methodCall.method == 'cancel') {
+      return standardMethod.encodeSuccessEnvelope(null);
+    } else {
+      fail('Expected listen or cancel');
+    }
+  });
 }
--- flutter/test/unit_test.dart
+++ flutter/test/unit_test.dart
@@ -1,7 +1,39 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:async';
+import 'dart:math';
 
-// import 'test_data.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:noise_meter/noise_meter.dart';
+
+import 'test_tools.dart';
 
 void main() {
-  test('', () async {});
+  group('Learning test', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    test('noise_meter', () async {
+      const MethodChannel('flutter.baseflow.com/permissions/methods')
+          .setMockMethodCallHandler((methodCall) async {
+        if (methodCall.method == 'requestPermissions') {
+          return <dynamic, dynamic>{
+            // Permission.microphone.request().isGranted(7) を True(1) にする
+            7: 1,
+          };
+        }
+        return null;
+      });
+      double getDb(double pcmValue) => 20 * log(pow(2, 15) * pcmValue) / ln10;
+      const data = <double>[1.0, 2.0, 3.0];
+      initializeFakeSensorChannel('audio_streamer.eventChannel', data);
+      NoiseMeter noiseMeter = NoiseMeter();
+      Stream<NoiseReading> stream = noiseMeter.noiseStream;
+      stream.listen((noiseReading) async {
+        debugPrint(noiseReading.toString());
+        await expectLater(stream.isBroadcast, isTrue);
+        expect(noiseReading.meanDecibel.round(), getDb(data.average).round());
+        expect(noiseReading.maxDecibel, getDb(data.max));
+      });
+    });
+  });
 }
