bcb3e180018dece97dae0f675024f2b0e171fffd
iori tobeta <15eb1137@gmail.com>
2022-12-08 16:46:15 +0900

    refactor tests

--- flutter/test/test_tools.dart
+++ flutter/test/test_tools.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 Widget wrap(Widget child) {
   return MaterialApp(home: Material(child: child));
@@ -31,3 +34,19 @@ void initializeFakeSensorChannel(String channelName, List<double> data) {
     }
   });
 }
+
+void initializeFakeRequestPermissions(Permission permission) {
+  const MethodChannel('flutter.baseflow.com/permissions/methods')
+      .setMockMethodCallHandler((methodCall) async {
+    if (methodCall.method == 'requestPermissions') {
+      return <dynamic, dynamic>{
+        // Permission.microphone.request().isGranted を True にする
+        permission.value: PermissionStatus.granted.index,
+      };
+    }
+    return null;
+  });
+}
+
+double getSignificantFigures(double x, int digit) =>
+    (x * pow(10, digit)).round() / pow(10, digit);
--- flutter/test/unit_test.dart
+++ flutter/test/unit_test.dart
@@ -2,38 +2,43 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:collection/collection.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:noise_meter/noise_meter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'test_tools.dart';
 
 void main() {
   group('Learning test', () {
-    TestWidgetsFlutterBinding.ensureInitialized();
+    final data = List<double>.generate(100, (_) => Random().nextDouble());
+
+    setUp(() {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      initializeFakeRequestPermissions(Permission.microphone);
+      initializeFakeSensorChannel('audio_streamer.eventChannel', data);
+    });
+
     test('noise_meter', () async {
       expect(NoiseMeter.sampleRate, 44100);
-      const MethodChannel('flutter.baseflow.com/permissions/methods')
-          .setMockMethodCallHandler((methodCall) async {
-        if (methodCall.method == 'requestPermissions') {
-          return <dynamic, dynamic>{
-            // Permission.microphone.request().isGranted(7) を True(1) にする
-            7: 1,
-          };
-        }
-        return null;
-      });
-      double getDb(double pcmValue) => 20 * log(pow(2, 15) * pcmValue) / ln10;
-      const data = <double>[1.0, 2.0, 3.0];
-      initializeFakeSensorChannel('audio_streamer.eventChannel', data);
+
       NoiseMeter noiseMeter = NoiseMeter();
       Stream<NoiseReading> stream = noiseMeter.noiseStream;
+
+      double getDb(double pcmValue) {
+        // const refSoundPress = 0.00002;
+        final inversed = pow(2, 15) * pcmValue;
+        double log10(double x) => log(x) * log10e;
+        return 20 * log10(inversed /* / refSoundPress */);
+      }
+
       stream.listen((noiseReading) async {
-        debugPrint(noiseReading.toString());
-        await expectLater(stream.isBroadcast, isTrue);
-        expect(noiseReading.meanDecibel.round(), getDb(data.average).round());
-        expect(noiseReading.maxDecibel, getDb(data.max));
+        expect(stream.isBroadcast, isTrue);
+        expect(
+            getSignificantFigures(noiseReading.meanDecibel, 5),
+            getSignificantFigures(
+                getDb(0.5 * (data.min.abs() + data.max.abs())), 5));
+        expect(getSignificantFigures(noiseReading.maxDecibel, 5),
+            getSignificantFigures(getDb(data.max), 5));
       });
     });
   });
